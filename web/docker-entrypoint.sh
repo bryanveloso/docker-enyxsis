cf0eab7f4975576d1d3cd44ab5f12363b73590b3
Bryan Veloso <bryan@avalonstar.com>
2020-08-30 17:05:23 -0700

    Just hardcode everything. >_>;

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -10,12 +10,5 @@ echo "http://rathena.org/board/"
 echo ""
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 echo "Initalizing Docker container..."
-
-sed -i "16s|127.0.0.1|database|" /var/www/fluxcp/config/servers.php
-sed -i "37s|127.0.0.1|database|" /var/www/fluxcp/config/servers.php
-sed -i "46s|127.0.0.1|login|" /var/www/fluxcp/config/servers.php
-sed -i "86s|127.0.0.1|char|" /var/www/fluxcp/config/servers.php
-sed -i "90s|127.0.0.1|map|" /var/www/fluxcp/config/servers.php
-
 echo "Starting FluxCP..."
 exec "$@"
