0701854c0971eaf5fb023e210655e711fbf59f87
Bryan Veloso <bryan@avalonstar.com>
2020-09-18 13:35:23 -0700

    Importing CORS would help.

--- status/app.js
+++ status/app.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const net = require('net');
+const express = require('express')
+const net = require('net')
+const cors = require('cors')
 
 const {
   CHAR_IP,
@@ -8,33 +9,33 @@ const {
   LOGIN_PORT = 6900,
   MAP_IP,
   MAP_PORT = 5121,
-} = process.env;
+} = process.env
 
 const checkPort = (port, host) => {
-  let result = null;
+  let result = null
   try {
     const client = new net.Socket()
     client.connect(port, host)
     result = true
     client.destroy()
   } catch (e) {
-    result = false;
+    result = false
   }
 
-  return result;
-};
+  return result
+}
 
-const app = express();
-app.use(cors());
+app.use(cors())
+const app = express()
 
 app.get('/', (req, res, next) => {
-  const login = checkPort(LOGIN_PORT, LOGIN_IP);
-  const char = checkPort(CHAR_PORT, CHAR_IP);
-  const map = checkPort(MAP_PORT, MAP_IP);
-  res.json({ status: { login, char, map } });
-});
+  const login = checkPort(LOGIN_PORT, LOGIN_IP)
+  const char = checkPort(CHAR_PORT, CHAR_IP)
+  const map = checkPort(MAP_PORT, MAP_IP)
+  res.json({ status: { login, char, map } })
+})
 
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3030
 app.listen(port, function () {
-  console.log(`Status listening on port ${port}!`);
-});
+  console.log(`Status listening on port ${port}!`)
+})
